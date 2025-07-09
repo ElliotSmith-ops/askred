5ba96d2f0b118153d61c8eaa3bcdf4c46b78b202
ElliotsmithUSC <elliotas@usc.edu>
2025-07-09 11:48:07 -0700

    coffee button fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -162,7 +162,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
+            <img
               src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
               alt="Buy Me A Coffee"
               style={{ height: "60px", width: "217px" }}
